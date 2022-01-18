061b26d7fb4b1c7aa776971744d479963cd6bbcb
EnotPotaskyn <ackii@bk.ru>
2022-01-19 01:19:27 +0300

    fix: fix some err

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if(n<0) {n = -n}
+    let str = String(n);
+    console.log(str)
+    let num = '';
+    for (let i=str.length-1;i>=0; i--){
+       num = num + str[i]  
+      }
+    
+      return Number(num)
 }
